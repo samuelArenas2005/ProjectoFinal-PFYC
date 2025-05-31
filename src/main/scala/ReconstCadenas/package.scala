c8a2954e37cbee6fb6d10e210bad86307e11d614
Carlos Castillo <crlscastillo161@gmail.com>
2025-05-30 20:15:28 -0500

    Implementación funcional de reconstruirCadenaMejorado con variables descriptivas

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -8,7 +8,28 @@ package object ReconstCadenas {
   }
 
   def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
-    Seq('c')
+    val vacia: Seq[Char] = Seq()
+
+    /** genera el conjunto de nuevas subcanedas de longitud k, enxtendiendo cada subcadena valida actual con cada caracter del alfabeto.
+     * nota: solo se conservan las subcadenas que el oraculo acepte. */
+    def generarSubcadenasValidas(subcadenasActuales: Set[Seq[Char]]): Set[Seq[Char]] =
+      for{
+        subcadena <- subcadenasActuales
+        letra <- alfabeto
+        nuevaSubcadena = subcadena :+ letra
+        if o(nuevaSubcadena)
+      } yield nuevaSubcadena
+
+    /** Construlle recursivamente el conjunto de subcadenass de longitud k validas*/
+    def construirSubcadenaValida(cadenasActuales:Set[Seq[Char]], k:Int):Seq[Char] ={
+        val siguientesSubcadenas = generarSubcadenasValidas(cadenasActuales)
+        siguientesSubcadenas.find(_.length == n) match{
+          case Some(sol) => sol
+          case None => construirSubcadenaValida(siguientesSubcadenas , k+1)
+        }
+    }
+    /** se  inicia la construccion desde la cadena vacia, con subcadenas de longitud 0 */
+    construirSubcadenaValida(Set(vacia),1)
   }
 
   def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
